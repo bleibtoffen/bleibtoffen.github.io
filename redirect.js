dca6dd1e8349b742537e894791dfadf6ea2d8410
Claas Augner <git@caugner.de>
2020-04-11 19:55:58 +0200

    feat: auto-redirect per navigator.language

---
+++ redirect.js
@@ -0,0 +1,14 @@
+switch (navigator.language) {
+    case "de":
+    case "de-DE":
+        document.getElementById('de').click();
+        break;
+
+    case "de-AT":
+        document.getElementById('at').click();
+        break;
+
+    case "de-CH":
+        document.getElementById('ch').click();
+        break;
+}
